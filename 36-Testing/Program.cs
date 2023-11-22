e5695d265bfdb7fa64c146d94f9af3177ff64750
ShootThePoop <147568900+ShootThePoop@users.noreply.github.com>
2023-11-22 12:39:22 +1100

    gfd

--- 36-Testing/Program.cs
+++ 36-Testing/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
@@ -16,10 +17,14 @@ namespace _36_Testing
         static void Main(string[] args)
         {
             // Write your code here
+
+
+
             Console.WriteLine("The Everything");
             Console.WriteLine("1. Name");
             Console.WriteLine("2. Calculator");
             Console.WriteLine("3. Miles to Kilometers or Kilometers to Miles");
+            Console.WriteLine("4. Fahrenheit to Celcius or Celcuis to Fahrenheit");
             string menu1 = Console.ReadLine();
 
 
@@ -38,24 +43,98 @@ namespace _36_Testing
                 MtoK();
             }
 
+            else if(menu1 == "4")
+            {
+                FtoC();
+            }
 
-            Exit();
+            else
+            {
+                Console.WriteLine("Invalid.");
+            }
+
+            Repeat();
+            
+
+            Schnitty();
+            
+
 
 
         }
 
+        private static void Repeat()
+        {
+            Console.ReadLine();
+            Console.WriteLine("Would you like to repeat the program?");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+            string repeat = Console.ReadLine();
 
-            // Wait for input before ending
+            if (repeat == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("The Everything");
+                Console.WriteLine("1. Name");
+                Console.WriteLine("2. Calculator");
+                Console.WriteLine("3. Miles to Kilometers or Kilometers to Miles");
+                Console.WriteLine("4. Fahrenheit to Celcius or Celcuis to Fahrenheit");
+                string menu1 = Console.ReadLine();
 
 
-            private static void Name()
+                if (menu1 == "1")
+                {
+                    Name();
+                }
+
+                else if (menu1 == "2")
+                {
+                    Calculator();
+                }
+
+                else if (menu1 == "3")
+                {
+                    MtoK();
+                }
+
+                else if (menu1 == "4")
+                {
+                    FtoC();
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid.");
+                }
+
+
+            }
+            else if (repeat == "2")
+            {
+                Exit();
+            }
+            
+
+        }
+
+        private static void Schnitty()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("sigmas dont eat schnitzel");
+        }
+
+
+
+
+        private static void Name()
+
             {
                 Console.Write("What is your first name? ");
                 string first = Console.ReadLine();
 
                 Console.Write("What is your last name? ");
                 string last = Console.ReadLine();
-                Console.WriteLine($"Hello {first} {last}");
+                Console.WriteLine($"Hello {first} {last}.");
             }
 
             private static void Exit()
@@ -84,7 +163,6 @@ namespace _36_Testing
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
 
-                Console.Clear();
                 Console.WriteLine("What would you like to do?");
                 Console.WriteLine("1. Kilometres to Miles");
                 Console.WriteLine("2. Miles to Kilometres");
@@ -108,6 +186,45 @@ namespace _36_Testing
                     Console.WriteLine($"{miles} miles to kilometers = {miles * 1.60934}");
                 }
 
+                
+            }
+            
+            private static void FtoC()
+            {
+            
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. Fahrenheit to Celsius");
+                Console.WriteLine("2. Celsius to Fahrenheit");
+                string FtoCchoice = Console.ReadLine();
+
+
+
+
+                if (FtoCchoice == "1")
+                {
+                    Console.Write("How many fahrenheit? ");
+                    double fr = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"{fr} fahrenheit to celsius = {(fr - 32) * 5 / 9}");
+                }
+
+                else if (FtoCchoice == "2")
+                {
+                    Console.Write("How many celsius? ");
+                    double cel = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"{cel} celsius to fahrenheit = {(cel * 9 / 5) + 32}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{FtoCchoice} is not a valid answer.");
+                }
+
+            {
+                
+            
+            }
             }
         
     }
